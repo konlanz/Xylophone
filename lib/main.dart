4d9110cdebb829bcb66dfefa8285013f22c4d468
konlanmikpekoah.km@outlook.com <konlanmikpekoah.km@outlook.com>
2021-01-11 14:44:59 +0000

    add the audioplayers package

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: SafeArea(child: Container(
-          child: Text('hellp'),
-        )),
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text(nouns.last),
+        ),
       ),
-    )
-  );
+    ),
+  ));
 }
-
