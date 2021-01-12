2cec19958bfdd4b1a9704ca4222d3b4dc2510637
konlanmikpekoah.km@outlook.com <konlanmikpekoah.km@outlook.com>
2021-01-12 04:49:58 +0000

    added the the 7 buttuns which plays

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: XyloPhone()
-  ));
+  runApp(MaterialApp(home: XyloPhone()));
 }
 
 class XyloPhone extends StatelessWidget {
@@ -14,18 +12,69 @@ class XyloPhone extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: FlatButton(
-            onPressed: () {
-              final player = AudioCache();
-              player.play('note1.wav');
-            },
-            child: Text('click Me'),
+        body: SafeArea(
+          child: Column(
+            children: [
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+                child: Text(''),
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+                child: Text(''),
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                },
+                child: Text(''),
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note4.wav');
+                },
+                child: Text(''),
+              ),
+              FlatButton(
+                color: Colors.greenAccent,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note5.wav');
+                },
+                child: Text(''),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note6.wav');
+                },
+                child: Text(' '),
+              ),
+              FlatButton(
+                color: Colors.pinkAccent,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note7.wav');
+                },
+                child: Text(' '),
+              ),
+            ],
           ),
         ),
       ),
-    ),
     );
   }
 }
